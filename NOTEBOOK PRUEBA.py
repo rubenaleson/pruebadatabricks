30900c0a92348767814b3962cd963bcf04b13efb
r.robles@aleson-itc.com <r.robles@aleson-itc.com>
2023-03-27 08:08:18 +0000

    Prueba1

---
+++ NOTEBOOK PRUEBA.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ESTO SIMPLEMENTE ES UN NOTEBOOK DE PRUEBA EN DATABRICKS
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC celda para comprobar el cambio  
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Nuevos cambios paa ver si funciona
